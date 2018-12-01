44c1042495bb7c998c1d94706a775b8e9e34d957
丁黎晓 <1713716789@qq.com>
2018-12-01 23:41:33 +0800

    示例

---
+++ basic_java/src/test/java/com/bitscott/FirstJUnit5Tests.java
@@ -0,0 +1,14 @@
+package com.bitscott;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FirstJUnit5Tests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
